dbaf6ffc582a08ebe7d3f4a4538a8ce5b0bcea36
lorencr7 <eden__lorenzo@hotmail.com>
2014-05-15 16:32:01 +0200

    Cambios en la forma de detectar colisionable

--- Shokoban/src/sd/emse/shokoban/Collision.java
+++ Shokoban/src/sd/emse/shokoban/Collision.java
@@ -10,17 +10,11 @@ public class Collision extends Shape {
 	public boolean collide(Shape shape1, Shape shape2) {
 		if (isCollisionable(shape1) && isCollisionable(shape2)) {
 			return true;
-
 		}
 		return false;
 	}
 
 	public boolean isCollisionable(Shape shape) {
-		if ((shape instanceof Box) || (shape instanceof Player)
-				|| (shape instanceof Wall)) {
-			return true;
-		}
-
-		return false;
+		return (shape instanceof Collision);
 	}
 }
